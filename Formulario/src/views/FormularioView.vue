c563386f82eaa029698a915859cd51d49aa96586
Alexander <alexandercantun744@gmail.com>
2024-01-29 21:25:50 -0500

    Se cambia nombre a carpeta

---
+++ Formulario/src/views/FormularioView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <p class="titulo">Completa el formulario</p>
+    <FormularioUsuario />
+  </main>
+</template>
+
+<script setup lang="ts">
+import FormularioUsuario from '../components/Menu/FormularioUsuario.vue'
+</script>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column; 
+  align-items: center; 
+}
+
+.titulo {
+  font-size: 24px;
+  margin-bottom: 20px; 
+}
+</style>
